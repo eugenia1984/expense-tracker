ba8a6018474d2f622de88ffe293a18d368a52f2e
eugenia1984 <costamariaeugenia1@gmail.com>
2024-10-21 14:33:27 +0200

    feat: add income and expense total

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="container">
     <Balance :total="total" />
-    <IncomeExpenses />
+    <IncomeExpenses :income="income" :expenses="expenses"/>
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
@@ -31,4 +31,19 @@ const total = computed(() => {
   }, 0);
 });
 
+// Get income
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) =>  acc + transaction.amount, 0)
+  .toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount < 0)
+  .reduce((acc, transaction) => acc + transaction.amount, 0)
+  .toFixed(2);
+});
 </script>
